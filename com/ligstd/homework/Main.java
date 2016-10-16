dcf73ee9d65b25fb7b3c8abac1e48c1b1f460981
greymon black <greymon black>
2016-10-16 16:34:36 +0800

    Initial Commit

--- com/ligstd/homework/Main.java
+++ com/ligstd/homework/Main.java
@@ -3,7 +3,7 @@ package com.ligstd.homework;
 import com.ligstd.homework.controllers.MainController;
 
 import java.io.IOException;
-
+//我爱软件工程
 public class Main {
 
     public static void main(String[] args) throws IOException {
